cb80890e1c4af2d23d3aaa0bae76b125bc486fd1
Andrea Delli <andrea.delli2001@gmail.com>
2025-09-06 23:26:35 +0200

    update script to test OpenVLA

--- test_openvla.py
+++ test_openvla.py
@@ -1,116 +1,119 @@
 import os
 import json
-import numpy as np
 import torch
-from tqdm import tqdm
 from PIL import Image
-from rlbench.environment import Environment
-from rlbench.action_modes.action_mode import MoveArmThenGripper
-from rlbench.observation_config import ObservationConfig
-from rlbench.tasks import PutRubbishInBin, PutBooksOnBookshelf, EmptyContainer
 from transformers import AutoModelForVision2Seq, AutoProcessor
+import argparse
+from LIFT3D.lift3d.envs.rlbench_env import RLBenchEvaluator, RLBenchActionMode
+import numpy as np
+import wandb
 
-from LIFT3D.lift3d.envs.rlbench_env import RLBenchActionMode, RLBenchObservationConfig, RLBenchEnv
 
+class OpenVLAPolicy(torch.nn.Module):
+    """Adapter for OpenVLA model to conform to RLBenchEvaluator policy interface."""
+    def __init__(self, processor, model, device):
+        super(OpenVLAPolicy, self).__init__()
+        self.processor = processor
+        self.model = model
+        self.device = device
 
-def test_openvla():
+    def forward(self, images, point_clouds, robot_states, texts):
+
+        # Convert tensor images [B, C, H, W] to PIL images
+        batch_size = images.shape[0]
+        pil_images = []
+        for i in range(batch_size):
+            img = images[i].permute(1, 2, 0).cpu().numpy().astype('uint8')
+            pil_images.append(Image.fromarray(img))
+
+            # Save image to disk
+            # img_path = f"image_{i}.png"
+            # pil_images[-1].save(img_path)
+
+        # Form prompts
+        prompts = [f"In: What action should the robot take to {text}?\nOut:" for text in texts]
+
+        # Tokenize and process images
+        inputs = self.processor(prompts, pil_images, return_tensors='pt', padding=True)
+        inputs = {k: v.to(self.device, dtype=torch.bfloat16) if v.is_floating_point() else v.to(self.device) for k, v in inputs.items()}
+
+        # Predict action
+        action = self.model.predict_action(**inputs, unnorm_key='PutRubbishInBin_euler_relative', do_sample=False) # TODO: CHANGE THIS BASED ON DATASET USED FOR FINETUNING
+
+        # TODO: TEST WITH HIGHER JUMPS
+        # action[0] *= 10
+        # action[1] *= 10
+        # action[2] *= 10
+
+        # Ensure tensor output on CPU
+        if isinstance(action, torch.Tensor):
+            return action.cpu()
+        return torch.tensor(action)
+
+def test_openvla(task_name, n_episodes):
     # Configuration
-    tasks = [PutRubbishInBin, PutBooksOnBookshelf, EmptyContainer]
-    checkpoint_path = "/home/adelli/RLBench/checkpoints/openvla-7b"
+    # checkpoint_path = "openvla/openvla-7b"
+    # checkpoint_path = "/home/adelli/RLBench/checkpoints/openvla-7b"
+    # checkpoint_path = "/home/adelli/openvla/checkpoints/openvla-7b+PutRubbishInBin_relative_euler+b10+lr-0.0005+lora-r32+dropout-0.0--image_aug"
+    # checkpoint_path = "/home/adelli/openvla/checkpoints/openvla-7b+PutRubbishInBin_absolute_euler+b10+lr-0.0005+lora-r32+dropout-0.0--image_aug"
+    checkpoint_path = "/home/adelli/openvla/checkpoints/openvla-7b+PutRubbishInBin_euler_relative+b10+lr-0.0005+lora-r32+dropout-0.0--image_aug"
     results_dir = "runs/openvla_test"
-    videos_dir = os.path.join(results_dir, "videos")
-    os.makedirs(videos_dir, exist_ok=True)
-    
+    os.makedirs(results_dir, exist_ok=True)
+
     # Load OpenVLA model
-    device = "cuda:1" if torch.cuda.is_available() else "cpu"
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     processor = AutoProcessor.from_pretrained(checkpoint_path, trust_remote_code=True)
     vla = AutoModelForVision2Seq.from_pretrained(
         checkpoint_path,
+        # attn_implementation="flash_attention_2",
         torch_dtype=torch.bfloat16,
         low_cpu_mem_usage=True,
         trust_remote_code=True
     ).to(device)
 
-    # Setup RLBench environment
-    obs_config = RLBenchObservationConfig.multi_view_config(
-        camera_names=["front", "wrist", "left_shoulder", "right_shoulder", "overhead"],
-        image_size=(256, 256)
-    )
+    # Load dataset stats used during finetuning (for action un-normalization)
+    dataset_statistics_path = os.path.join(checkpoint_path, "dataset_statistics.json")
+    if os.path.isfile(dataset_statistics_path):
+        print(f"Loading dataset statistics from {dataset_statistics_path}")
+        with open(dataset_statistics_path, "r") as f:
+            norm_stats = json.load(f)
+        vla.norm_stats = norm_stats
 
-    action_mode = RLBenchActionMode.eepose_then_gripper_action_mode(absolute=True)
+    policy = OpenVLAPolicy(processor, vla, device)
+    max_steps = 150
 
-    env = RLBenchEnv(
-        task_name="put_rubbish_in_bin", # Will be changed in the loop
-        action_mode=action_mode,
-        obs_config=obs_config,
-        headless=True,
-        cinematic_record_enabled=False,
-        verbose_warnings=True,
-    )
-    
+    os.environ['LIBGL_ALWAYS_SOFTWARE'] = 'true'
+
     results = {}
-    
-    for task_class in tasks:
-        task_name = task_class.__name__
-        task_name = ''.join(['_' + i.lower() if i.isupper() else i for i in task_name]).lstrip('_')
-        env.task_name = task_name
-        env.env.task_class = task_class
-        print(f"Testing task: {task_name}")
-        
-        n_episodes = 10
-        max_steps = 100
-        success_rate = 0
-        
-        for i in tqdm(range(n_episodes), desc=f"Running {task_name} episodes"):
-            obs = env.reset()
-            
-            # Get language instruction
-            instruction = env.text
-            # Print step information
-            print(f"Observation: {obs}")               
-            
-            for step in range(max_steps):
-                # Get action from OpenVLA
-                image = Image.fromarray(obs['image'])
-                prompt = f"In: What action should the robot take to {instruction}?\nOut:"
-                inputs = processor(prompt, image).to(device, dtype=torch.bfloat16)
+    print(f"Testing task: {task_name}")
+    evaluator = RLBenchEvaluator(
+        task_name=task_name,
+        image_size=256,
+        action_mode=RLBenchActionMode.eepose_then_gripper_action_mode(absolute=False),
+        camera_name="left_shoulder",
+        point_cloud_camera_names=[],
+        use_point_crop=True,
+        num_points=1024,
+        max_episode_length=max_steps,
+        rotation_representation='euler',
+        headless=True,
+        verbose_warnings=True,
+        cinematic_record_enabled=True
+    )
+    success_rate, avg_rewards = evaluator.evaluate(n_episodes, policy, verbose=True, verbose_with_cinematic=True)
+    evaluator.callback_verbose(wandb_logger=wandb)
+    print(f"Task {task_name}: success_rate={success_rate}, avg_rewards={avg_rewards}")
+    results[task_name] = {"success_rate": success_rate, "avg_rewards": avg_rewards}
 
-                # Predict the action
-                # "maniskill_dataset_converted_externally_to_rlds" is the dataset in Open-X with the highest number of samples with Franka robot
-                # Other options: 'viola', 'taco_play'
-                action = vla.predict_action(**inputs, unnorm_key='toto', do_sample=False)
-                
-                print(f"Step {step + 1}/{max_steps}, Action: {action}")
-                
-                # Step the environment
-                obs, reward, terminate, truncated, _ = env.step(action)
-                
-                
-                print(f"Truncated: {truncated}, terminated: {terminate}, Reward: {reward}, Step: {step + 1}/{max_steps}")
-                
-                # Check for success or termination
-                if terminate or truncated:
-                    print(f"Episode {i} ended with {'success' if reward > 0 else 'failure'}")
-                    if reward > 0:
-                        success_rate += 1
-                    break
-                
-            if step == max_steps - 1:
-                print(f"Episode {i} reached max steps without termination.")
-            else:
-                print(f"Episode {i} terminated at step {step} with reward {reward}.")
-            
-            # Save video
-            video_path = os.path.join(videos_dir, f"{task_name}_episode_{i}.mp4")
-            env.tr.save(video_path, instruction)
-
-        results[task_name] = {"success_rate": success_rate / n_episodes}
-        
     # Save results
-    with open(os.path.join(results_dir, "results.json"), "w") as f:
+    with open(os.path.join(results_dir, f"results_{task_name}.json"), "w") as f:
         json.dump(results, f, indent=4)
-        
-    env.env.shutdown()
 
 if __name__ == "__main__":
-    test_openvla()
+
+    parser = argparse.ArgumentParser(description="Test OpenVLA policy on a specific RLBench task")
+    parser.add_argument("task_name", type=str, choices=["put_rubbish_in_bin", "put_books_on_bookshelf", "empty_container"], help="Name of the RLBench task to test")
+    parser.add_argument("n_episodes", type=int, default=10, help="Number of episodes to run for evaluation")
+    args = parser.parse_args()
+    wandb.init(project="vla-eval", entity="andrea-delli-politecnico-di-torino", name=f"openvla-{args.task_name}")
+    test_openvla(args.task_name, args.n_episodes)
